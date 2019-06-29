3939c37900455af5464faae0a3a2ba6b19738e7d
Vladimir Pesotskiy <developer@d3en.ru>
2019-06-29 16:05:53 +0600

    created Utils.parseFullName()

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?) = when (fullName) {
+        null, "", " " -> null to null
+        else -> {
+            val parts: List<String>? = fullName.split(" ")
+            val firstName = parts?.getOrNull(0)
+            val lastName = parts?.getOrNull(1)
+            firstName to lastName
+        }
+    }
+}
