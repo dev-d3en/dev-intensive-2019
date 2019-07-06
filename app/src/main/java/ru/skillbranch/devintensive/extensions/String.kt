d4dd6f400befaa1c76bbcb938230c77965cdb8c1
Vladimir Pesotskiy <developer@d3en.ru>
2019-07-06 14:21:35 +0600

    created String.truncate()

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(count: Int = 16):String {
+    val trimmed = this.trim()
+    return if (trimmed.length > count) {
+        val substr = trimmed.substring(0..count-1)
+        if (substr.last() == ' ') {
+            substr.dropLast(1).plus("...")
+        } else {
+            substr.plus("...")
+        }
+    } else trimmed
+}
